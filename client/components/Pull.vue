8dc7f1b4a4034788705e641333513ea05ae99461
AnastasiaMik <Anastasia081998@gmail.com>
2019-04-29 02:44:35 +0300

    Create Pull.vue
    Создаю файл Vue для всех док-ов:
    1) для начала для каждого док-а в качестве ключа устанавливаю ID
    2) добавляю кнопку для нового док-а
    3) вызываю массив из док-ов и функции для добавления новых

---
+++ client/components/Pull.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="pull">
+        <h3>Markup docs</h3>
+        <ul>
+            <li v-for="docs in pull" v-bind:key="docs._id">
+                <a href="#" v-on:click="onPress(docs)">{{docs.title}}</a>
+            </li>
+            <button style="margin-top: 20px" v-on:click="onAddNew()">Add new</button>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Pull",
+        props: {
+            pull: Array,
+            onPress: Function,
+            onAddNew: Function
+        }
+    };
+</script>
+
+<style scoped>
+    ul {
+        padding: 0;
+    }
+    .pull {
+        flex: 1;
+        max-width: 220px;
+        border-right: 1px solid black;
+        padding: 16px;
+    }
+</style>
