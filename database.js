eb0ff227e3469a9c9b0e37fafb9441a6db35371f
AnastasiaMik <Anastasia081998@gmail.com>
2019-04-29 02:46:30 +0300

    Update database.js

--- database.js
+++ database.js
@@ -1,26 +1,24 @@
-const mongoose = require('mongoose');
-let Schema = mongoose.Schema;
-let docsSchema =new Schema({
-    title:  String,
-    markup: String,
-
-});
-
-
-let Docs = mongoose.model("docs", docsSchema);
-
-module.exports.Docs = Docs;
+var mongoose = require("mongoose");
 
 mongoose.connect(
     'mongodb://127.0.0.1:27017/docs',
-    { useNewUrlParser: true}
-).then(() => {
-    Docs = mongoose.model('docs', new mongoose.Schema(docsSchema));
-});
+     { useNewUrlParser: true}
+);
 
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function() {
     console.log("Successfully connected to DB");
 });
+
+var mongoose = require("mongoose");
+
+var docsSchema = new mongoose.Schema({
+    title: String,
+    markup: String
+});
+
+var Docs = mongoose.model("Docs", docsSchema);
+
+module.exports.Docs = Docs;
 
