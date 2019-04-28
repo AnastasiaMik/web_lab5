1b1805a72aac71daf9fbdd7cc925fdebc60ad659
AnastasiaMik <Anastasia081998@gmail.com>
2019-04-29 01:43:33 +0300

    Create www.js
    Создаю HTTP сервер:
    1) прослушиваются все порты
    2) мониторинг ошибок

---
+++ bin/www.js
@@ -0,0 +1,11 @@
+var server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+}
